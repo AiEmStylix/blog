5ac93e8bbe8d7152482839aa12fd36efd8f791f6
AiEmStylix <anhduongfa@gmail.com>
2025-10-14 11:34:05 +0700

    refactor: rewrite backend to use postgresjs

--- apps/backend/src/routes/categories.ts
+++ apps/backend/src/routes/categories.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import db from '../database/db';
 import slugify from '@sindresorhus/slugify';
+import sql from '../database/db';
 
 const router = Router();
 
@@ -10,7 +11,7 @@ interface Category {
   slug: string;
 }
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
     if (!req.body || typeof req.body.name !== 'string' || req.body.name.trim() === '') {
       return res.status(400).json({ error: 'Category name is required' });
@@ -18,43 +19,44 @@ router.post('/', (req: Request, res: Response, next: NextFunction) => {
     const { name } = req.body;
     console.log(typeof name);
     const slug = slugify(name);
-    const stmt = db.prepare('INSERT INTO categories (name, slug) VALUES (?, ?)');
 
-    const result = stmt.run(name, slug);
+    const [category] = await sql<Category[]>` INSERT INTO categories (name, slug)
+      VALUES (${name}, ${slug})
+      RETURNING id, name, slug`;
 
-    res.status(201).json({
-      id: result.lastInsertRowid,
-      name,
-      slug,
-    });
+    res.status(201).json(category);
   } catch (error) {
     next(error);
   }
 });
 
 // GET /categories
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const stmt = db.prepare('SELECT * FROM categories ORDER BY name');
-    const categories = stmt.all() as Category[];
+    const categories = await sql<Category[]>`
+    SELECT * FROM categories ORDER BY name`;
+
     res.json(categories);
   } catch (error) {
     next(error);
   }
 });
 
-router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const id = Number(req.params.id);
-    const stmt = db.prepare('SELECT * FROM categories WHERE id = ?');
-    const result = stmt.get(id) as Category;
-    return res.json(result);
+    const [category] = await sql<Category[]>`SELECT * FROM categories WHERE id = ${id}
+    `;
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    return res.json(category);
   } catch (error) {
     next(error);
   }
 });
 
-router.patch('/:id', (req: Request, res: Response, next: NextFunction) => {
+router.patch('/:id', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const id = Number(req.params.id);
 
@@ -63,8 +65,7 @@ router.patch('/:id', (req: Request, res: Response, next: NextFunction) => {
     }
 
     //Check if category exists
-    const checkStmt = db.prepare('SELECT * FROM categories WHERE id = ?');
-    const existing = checkStmt.get(id) as Category;
+    const [existing] = await sql<Category[]>`SELECT * FROM categories WHERE id = ${id}`;
 
     if (!existing) {
       return res.status(404).json({ error: 'Category not found' });
@@ -80,27 +81,26 @@ router.patch('/:id', (req: Request, res: Response, next: NextFunction) => {
     }
 
     //Update to the db
-    const stmt = db.prepare('UPDATE categories SET name = ?, slug = ? WHERE id = ?');
-    const result = stmt.run(name, slug, id);
+    const [updated] = await sql<
+      Category[]
+    >`UPDATE categories SET name = ${name}, slug = ${slug} WHERE id = ${id}`;
 
-    const updatedCategory = checkStmt.get(id);
-    res.json(updatedCategory);
+    res.json(updated);
   } catch (error) {
     next(error);
   }
 });
 
-router.delete('/:id', (req: Request, res: Response, next: NextFunction) => {
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const id = Number(req.params.id);
     if (!Number.isInteger(id)) {
       return res.status(400).json({ error: 'Invalid category id' });
     }
 
-    const stmt = db.prepare('DELETE FROM categories WHERE id = ?');
-    const result = stmt.run(id);
+    const result = await sql`DELETE FROM categories WHERE id = ${id}`;
 
-    if (result.changes === 0) {
+    if (result.count === 0) {
       return res.status(404).json({ error: 'Category not found' });
     }
 
