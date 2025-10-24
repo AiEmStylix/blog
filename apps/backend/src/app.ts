83aef5e8867f0ef9fe900e8d5496c5d00c40bff0
AiEmStylix <anhduongfa@gmail.com>
2025-10-25 03:04:54 +0700

    fix: changing database url

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -11,6 +11,7 @@ app.use('/api/posts', postsRoutes);
 app.use('/api/categories', categoriesRouter);
 
 if (process.env.NODE_ENV === 'production') {
+  console.log(__dirname);
   const staticPath = path.resolve(__dirname, '../../../frontend/dist');
   console.log('🧱 Serving static from:', staticPath);
   app.use(express.static(staticPath));
