92f3f9e705dd28be58c6db814b90026c5d2940b1
Stylix <anhduongfa@gmail.com>
2026-02-21 02:02:27 +0700

    feat: adding wrangler for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import markdoc from '@astrojs/markdoc';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [markdoc()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
