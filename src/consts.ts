c37896b7527eac9c7e5132a9109f6d9d706f7e5b
Stylix <anhduongfa@gmail.com>
2026-02-22 14:38:47 +0700

    feat: adding meta tag for better seo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "SYSTEM BLOG / [STYLIX]";
-export const SITE_DESCRIPTION = "A dreamy boy in a pragmatic world!";
+export const SITE_DESCRIPTION = "A dreamy tech boy in a pragmatic world!";
